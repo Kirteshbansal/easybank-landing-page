b1dbf87d5e05a46ba472539a0e5833276676ce3f
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-22 15:26:43 +0530

    Add script.js file

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger1 = document.getElementById("hamburger1");
+const hamburger2 = document.getElementById("hamburger2");
+const show = document.getElementById("show");
+
+function openNav() {
+  hamburger2.style.display = "block";
+  hamburger1.style.display = "none";
+  show.style.display = "block";
+}
+function closeNav() {
+  hamburger1.style.display = "block";
+  hamburger2.style.display = "none";
+  show.style.display = "none";
+}
+
+hamburger1.addEventListener("click", openNav);
+hamburger2.addEventListener("click", closeNav);
+window.addEventListener("scroll", closeNav);
